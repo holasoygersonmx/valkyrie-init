2f63a29a5f95e0b20674b35187c1bd6173d698dc
Gersón Gómez <gergomez18@gmail.com>
2022-12-10 22:52:28 -0500

    Actualización

--- layouts/head.php
+++ layouts/head.php
@@ -18,7 +18,7 @@
 		{$dependencies.css}
 	</head>
 	<body class="pos-relative">
-		<header id="desktop_menu" class="pos-fixed p-tb-20">
+		<header id="desktop_menu" class="wth-100 pos-fixed p-tb-20">
 			<div class="container">
 				<div class="row">
 					<div class="col-4 col-md-2">
@@ -33,7 +33,7 @@
 						<nav>
 							<ul class="d-flex align-items-center justify-content-end list-unstyled">
 								<li class="m-r-20"><a href="#home">{$lang.home}</a></li>
-								<li class="m-r-20"><a href="#contact_us">{$lang.contact_us}</a></li>
+								<li class="m-r-20"><a href="#contact">{$lang.contact}</a></li>
 								<li class="m-r-20"><a href="<?php echo Configuration::$vars['rrss']['facebook']['url']; ?>" target="_blank"><i class="fa-brands fa-facebook-square"></i></a></li>
 								<li class="m-r-20"><a href="<?php echo Configuration::$vars['rrss']['instagram']['url']; ?>" target="_blank"><i class="fa-brands fa-instagram"></i></a></li>
 								<li class="m-r-20"><a href="<?php echo Configuration::$vars['rrss']['whatsapp']['url']; ?>" target="_blank"><i class="fa-brands fa-whatsapp"></i></a></li>
@@ -52,12 +52,12 @@
 				</div>
 			</div>
 		</header>
-		<header id="mobile_menu" class="pos-fixed d-flex align-items-center justify-content-center p-20" data-mobile-flex>
+		<header id="mobile_menu" class="hth-vh pos-fixed d-flex align-items-center justify-content-center p-20" data-mobile-flex>
 			<a data-action="close_mobile_menu" class="pos-absolute"><i class="fas fa-times"></i></a>
 			<nav>
 				<ul class="d-flex align-items-center flex-column list-unstyled">
 					<li class="m-b-20"><a href="#home">{$lang.home}</a></li>
-					<li class="m-b-20"><a href="#contact_us">{$lang.contact_us}</a></li>
+					<li class="m-b-20"><a href="#contact">{$lang.contact}</a></li>
 					<li class="m-b-20">
 						<a href="<?php echo Configuration::$vars['rrss']['facebook']['url']; ?>" target="_blank" class="m-r-10"><i class="fa-brands fa-facebook-square"></i></a>
 						<a href="<?php echo Configuration::$vars['rrss']['instagram']['url']; ?>" target="_blank" class="m-lr-10"><i class="fa-brands fa-instagram"></i></a>
